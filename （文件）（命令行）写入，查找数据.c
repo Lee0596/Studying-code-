932b805a1e8cf4ea51ec9378ec54d450c085b3db
William-it <44289065+William-it@users.noreply.github.com>
2018-12-30 16:10:32 +0800

    Create （文件）（命令行）写入，查找数据.c
    功能如下：
    1）用键盘输入若干个整数，以二进制文件的形式保存在数据文件d.dat中。
    2）用键盘输入一个整数，在文件中找出该整数并输出位置（处在第几个数），若找不到则输出“NOT FOUND!”.
    3）采用命令行带参数的形式，实现2）的查找功能。

---
+++ （文件）（命令行）写入，查找数据.c
@@ -0,0 +1,61 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+int main(int argc, char *argv[]) {         //命令行参数
+	FILE *fp;
+	int n, j;
+	while (1) {
+		printf("1:输入若干个整数，并保存到文件\n");
+		printf("2:打开数据文件，并查找指定数\n");
+		printf("3:退出程序\n");           //功能菜单
+		scanf("%d", &n);            //功能选择
+		if(n==1)              //写入数据
+		{
+			if ((fp = fopen("E:\\C++\\text\\data.dat", "wb")) == NULL) {
+				printf("File open error!\n");
+				exit(0);
+			}
+			printf("请输入若干个整数，每个整数间用空格隔开，按^Z结束输入：\n");
+			while (~scanf("%d", &j)) {
+				fwrite(&j, sizeof(int), 1, fp);
+			}
+			if (fclose(fp)) {
+				printf("Can not close the file!\n");
+				exit(0);
+			}
+			continue;
+		}
+		if (n == 2)          //查找数据
+		{   
+			int data, f, flag = 1, i = 0;
+			if ((fp = fopen("E:\\C++\\text\\data.dat", "rb")) == NULL) {
+				printf("File open error!\n");
+				exit(0);
+			}
+			printf("请输入您要寻找的数：");
+			scanf("%d", &f);
+			while (!feof(fp)) {
+				fread(&data, sizeof(int), 1, fp);
+				if (data == f) {
+					flag = 0;
+					printf("%d位于第%d个数\n", f, i);
+				}
+				i++;
+			}
+			if (flag) {                //没有找到指定数据
+				printf("NOT FOUND!\n");        
+			}
+			if (fclose(fp)) {
+				printf("Can not close the file!\n");
+				exit(0);
+			}
+			continue;
+	    }
+		if (n == 3)          //退出程序
+		{
+			break;
+		}
+	}
+
+
+	return 0;
+}
